1b7f0913c9c7fa2685662c84f0ecad40186bff9a
1l8c3 <alice.mycoudert@gmail.com>
2024-05-28 15:57:03 +0200

    form 2

--- form.php
+++ form.php
@@ -7,14 +7,15 @@
 </head>
 <body>
     <h2>Formulaire d'Inscription</h2>
-    <form action="traitement_co.php" method="post">
-        <label for="identifiant">Identifiant :</label>
-        <input type="text" id="identifiant" name="identifiant" required><br><br>
+    <form action="traitement_inscription.php" method="post">
+        
+        <label for="courriel">Identifiant :</label>
+        <input type="email" id="courriel" name="courriel" required><br><br>
 
         <label for="motdepasse">Mot de passe :</label>
         <input type="password" id="motdepasse" name="motdepasse" required><br><br>
 
-        <button type="submit">Se connecter</button>
+        <button type="submit">S'inscrire</button>
     </form>
 </body>
 </html>
