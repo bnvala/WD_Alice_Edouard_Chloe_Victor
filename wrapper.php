ab8f76d8d9c4ac1602f655ea07f51cc96a2114c6
1l8c3 <alice.mycoudert@gmail.com>
2024-05-29 17:38:22 +0200

    commit wrapper

---
+++ wrapper.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles_entete.css">
+    </head>    
+    <body>
+<div id="wrapper">
+        <header>
+            <h1>Omnes Immobilier</h1>
+            <nav>
+                <ul>
+                    <li><a href="accueil.php">Accueil</a></li>
+                    <li><a href="toutparcourir.php">Tout Parcourir</a></li>
+                    <li><a href="recherche.php">Recherche</a></li>
+                    <li><a href="rendez-vous.php">Rendez-vous</a></li>
+                    <li><a href="form.php">Votre Compte</a></li>
+                </ul>
+            </nav>
+        </header>
+</div>
+</body>
