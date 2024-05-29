cf1f6b97bc237df8bd2aae1a45f8e2c604081cdb
1l8c3 <alice.mycoudert@gmail.com>
2024-05-29 19:34:03 +0200

    rere

--- toutparcourir.php
+++ toutparcourir.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tout Parcourir</title>
+    <link rel="stylesheet" type="text/css" href="styles_toutparcourir.css">
 </head>
 <body>
 <?php include 'wrapper.php'; ?>
