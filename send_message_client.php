2acfaa7631cc1d882367c111fa1904b3f4945bdc
edouardmnt <edouardmenut04@gmail.com>
2024-06-02 17:25:37 +0200

    send_message_client

--- send_message_client.php
+++ send_message_client.php
@@ -1,25 +1,23 @@
 <?php
-// Informations de connexion à la base de données
+// mot de passe identifiant bdd 
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "pj_piscine";
 
-// Connexion à la base de données
+// connexion bdd
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérifier la connexion
+// verification de connection à la bdd 
 if ($conn->connect_error) {
     die("Échec de la connexion : " . $conn->connect_error);
 }
-
-// Traitement de la soumission du formulaire
+// requete sql pour ajouter un message 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_agent = $_POST['id_agent'];
     $id_client = $_POST['id_client'];
     $message = $_POST['message'];
 
-    // Requête SQL pour insérer la communication dans la base de données
     $stmt = $conn->prepare("INSERT INTO communication (ID_client, ID_agent, message, envoyeur) VALUES (?, ?, ?, 'client')");
     $stmt->bind_param("iis", $id_client, $id_agent, $message);
 
@@ -27,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo json_encode([
             'success' => true,
             'message' => $message,
-            'timestamp' => date('Y-m-d H:i:s') // Vous pouvez utiliser le timestamp réel de la base de données si nécessaire
+            'timestamp' => date('Y-m-d H:i:s')
         ]);
     } else {
         echo json_encode([
