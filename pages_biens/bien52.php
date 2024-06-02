6a5208e36d92bad9e15d8edb056348499c99e12f
edouardmnt <edouardmenut04@gmail.com>
2024-06-02 18:44:21 +0200

    bien

--- pages_biens/bien52.php
+++ pages_biens/bien52.php
@@ -6,76 +6,95 @@
     <title>Présentation du Bien</title>
     <link rel="stylesheet" type="text/css" href="biens.css">
     <link rel="stylesheet" type="text/css" href="../styles_entete.css">
-    <?php include '../wrapper_biens.php'; ?>  
 </head>
 <body>
+<?php include '../wrapper_biens.php'; ?>  
     <h1 class="title">Présentation du Bien</h1>
     <div class="container">
         <div class="image">
             <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "pj_piscine";
+            // mdp id BDD
+            $servername = "localhost"; 
+            $username = "root";        
+            $password = "";           
+            $dbname = "pj_piscine";    
 
+            // Connexion BDD
             $conn = new mysqli($servername, $username, $password, $dbname);
 
+            // Vérifier la connexion
             if ($conn->connect_error) {
                 die("Échec de la connexion : " . $conn->connect_error);
             }
 
+            // ID du bien à afficher
             $id_bien = 52;
 
+            // Requête SQL pour récupérer les informations du bien
             $sql = "SELECT * FROM biens WHERE id = $id_bien";
             $result = $conn->query($sql);
 
+            // Vérifier s'il y a des résultats
             if ($result->num_rows > 0) {
+                // Récupérer les données du bien
                 $row = $result->fetch_assoc();
                 $photo = "../" . $row["photos"];
+                // Afficher l'image du bien
                 echo "<img src='$photo' alt='Photo du bien'>";
             } else {
                 echo "Aucun bien trouvé avec cet ID.";
             }
 
+            // Fermer la connexion
             $conn->close();
             ?>
         </div>
         <div class="info">
             <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "pj_piscine";
+            // id mdp BDD
+            $servername = "localhost"; 
+            $username = "root";        
+            $password = "";           
+            $dbname = "pj_piscine";    
 
+            // Connexion BDD
             $conn = new mysqli($servername, $username, $password, $dbname);
 
+            // Vérifier la connexion
             if ($conn->connect_error) {
                 die("Échec de la connexion : " . $conn->connect_error);
             }
 
-            $id_bien = 52;
+            // ID du bien à afficher
+            $id_bien = 51;
 
+            // Requête SQL pour récupérer les informations du bien 
             $sql = "SELECT * FROM biens WHERE id = $id_bien";
             $result = $conn->query($sql);
 
+            // Vérifier s'il y a des résultats
             if ($result->num_rows > 0) {
+                // Récupérer les données du bien
                 $row = $result->fetch_assoc();
                 $type = $row["type"];
                 $description = $row["description"];
                 $adresse = $row["adresse"];
-                $prix = $row["prix"];
+                $prix = $row["prix"]; 
         
+                // Afficher les informations du bien
                 echo "<h2>Type: $type</h2>";
                 echo "<p>Description: $description</p>";
                 echo "<p>Adresse: $adresse</p>";
-                echo "<p>Prix: $prix €</p>";
-               ?>
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.2660044358527!2d3.014740376473815!3d50.640750771629115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c32aa785fa98bd%3A0x83317c7db567a5b3!2s10%20Av.%20de%20la%20R%C3%A9publique%2C%2059160%20Lille!5e0!3m2!1sfr!2sfr!4v1716993651194!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                <?php 
-           } else {
-               echo "Aucun bien trouvé avec cet ID.";
-           }
+                echo "<p>Prix: $prix €</p>"; 
+                ?>
+                <!-- google map du lieux du bien -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2901.173849966718!2d5.370740076052417!3d43.35247397111797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c018a8d103eb%3A0xddb1381652923721!2s20%20Rue%20des%20Champs%2C%2013015%20Marseille!5e0!3m2!1sfr!2sfr!4v1716993503468!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                 <?php 
+            } else {
+                echo "Aucun bien trouvé avec cet ID.";
+            }
 
-           ?>
+            ?>
         </div>
     </div>
 
@@ -83,20 +102,23 @@
         <h2>Agents Immobiliers Spécialisés</h2>
         <div class="agents">
             <?php
+            // Requête SQL pour récupérer les agents immobiliers en fonction du type du bien
             $sql_agents = "SELECT * FROM agent WHERE specialite = '$type'";
             $result_agents = $conn->query($sql_agents);
 
+            // Vérifier s'il y a des résultats
             if ($result_agents->num_rows > 0) {
                 while($row_agents = $result_agents->fetch_assoc()) {
                     echo '<div class="agent-card" onclick="window.location.href=\'../profil-agent.php?id=' . $row_agents["id_agent"] . '\'">';
-                    echo '<img src="../photos_agents/' . $row_agents["photo"] . '" alt="Photo de l\'agent'>";
+                    echo '<img src="../photos_agents/' . $row_agents["photo"] . '" alt="Photo de l\'agent">';
                     echo '<div class="specialty"> ' . $row_agents["nom"] . '</div>';
                     echo '<div class="specialty"> ' . $row_agents["prenom"] . '</div>';
                     echo '</div>';
                 }
             } else {
-                echo "No agents found";
+                echo "pas d'agent trouver";
             }
+            // Fermer la connexion
             $conn->close();
             ?>
         </div>
